b4477b94ccaa7ca2be8928af3106ddb9aa9d385c
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-02-22 16:01:22 +0100

    Add v11.0.1.1 backport

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name="clang-format-precommit-dummy",
     version="0.0.0",
-    install_requires=["clang-format==10.0.1.1"]
+    install_requires=["clang-format==11.0.1.1"]
 )
