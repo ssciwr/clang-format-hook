542bae4b7e39a20adace9ec45420bc7200c42d2c
Dominic Kempf <dokempf@users.noreply.github.com>
2022-04-13 12:14:35 +0200

    clang-format 14.0.1

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name="clang-format-precommit-dummy",
     version="0.0.0",
-    install_requires=["clang-format==14.0.0"]
+    install_requires=["clang-format==14.0.1"]
 )
