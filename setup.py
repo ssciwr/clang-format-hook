a16216cc831c92edce0e67c137602938993bc714
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2023-03-21 13:12:44 +0100

    clang-format v16.0.0

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name="clang-format-precommit-dummy",
     version="0.0.0",
-    install_requires=["clang-format==15.0.6"]
+    install_requires=["clang-format==16.0.0"]
 )
